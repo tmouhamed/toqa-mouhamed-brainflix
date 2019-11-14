815a26c803e82d11e0ace296af34202bb41be9da
tmouhamed <s.toqaa@gmail.com>
2019-11-14 05:37:44 -0500

    data files are setup on the server

--- sprint3/serverSide/server.js
+++ sprint3/serverSide/server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
+const videoRoute = require('./routes/videoList');
+
+const app = express();
+
+app.use(cors());
+app.use('/videos', videoRoute);
+
+app.listen(8080, () => {
+    console.log('======= The Server is working=======');
+})
